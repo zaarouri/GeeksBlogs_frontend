28c138bae997b996c2d762ccf1b4705874affd13
Abdelmounime <mac@192.168.11.126>
2025-06-03 21:43:37 +0100

    v4

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import './Navbar.css';
+import logo from '../assets/geeks-logo.jpg'; // Make sure the image exists in this path
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="logo">GeeksBlogs</div>
+      {/* Left side links */}
       <ul className="nav-links">
-        <li><a href="#intro">Intro</a></li>
-        <li><a href="#features">Features</a></li>
-        <li><a href="#examples">Examples</a></li>
+        <li><a href="#intro"><i className="bi bi-house-door"></i> Intro</a></li>
+        <li><a href="#features"><i className="bi bi-grid"></i> Features</a></li>
+        <li><a href="#examples"><i className="bi bi-images"></i> Examples</a></li>
+      </ul>
+
+      {/* Center logo */}
+      <div className="logo">
+        <img src={logo} alt="GeeksBlogs Logo" className="logo-img" />
+      </div>
+
+      {/* Right side icons */}
+      <ul className="nav-links">
+        <li><i className="bi bi-person-circle"></i></li>
+        <li><i className="bi bi-bell"></i></li>
+        <li><i className="bi bi-gear"></i></li>
       </ul>
     </nav>
   );
