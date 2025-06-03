110eab2580b9b7cb30e4b7123951a67d31197823
chihab eddine yassir <chihab.eddine.yassir123@gmail.com>
2025-06-03 20:55:48 +0100

    features created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Intro from '../src/components/intro';
+import Features from '../src/components/Features';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Intro />
+      < Features/>
+      
     </div>
   );
 }
---
+++ src/components/Features.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import image from '../assets/img2.jpg';
+import './Features.css';
+
+function Features() {
+  return (
+    <article className="recent-posts-article">
+      <div className="post-img">
+        <img
+          src={image}
+          alt="Climatiseur split mural"
+          className="img-fluid"
+        />
+      </div>
+      <h4 className="text-center" style={{ margin: 0 }}>
+        Climatiseurs Split Inverter
+      </h4>
+      <p className="description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.
+      </p>
+    </article>
+  );
+}
+
+export default Features;
